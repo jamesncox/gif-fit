79dca1f7e70101afbc40714aea6511131c2cdca6
jamesncox <jamesncox@yahoo.com>
2020-07-25 21:04:00 -0500

    Create isActive reducer and actionTypes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import params from './params'
+import isActive from './isActive'
 
 
 const rootReducer = combineReducers(
     {
-        params
+        params,
+        isActive
     }
 )
 
