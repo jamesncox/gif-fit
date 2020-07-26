b9b0f512f2df0df76df7349b9f6fc98097b868c5
jamesncox <jamesncox@yahoo.com>
2020-07-25 21:08:22 -0500

    Add isActiveTrue() to StartWorkout

--- src/components/Layout/StartWorkout.js
+++ src/components/Layout/StartWorkout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { IS_ACTIVE_TRUE } from '../../actionTypes'
 import Button from '@material-ui/core/Button';
 import { deepPurple } from '@material-ui/core/colors';
+import { connect } from 'react-redux';
 
 const ColorButton = withStyles((theme) => ({
     root: {
@@ -27,14 +29,29 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function StartWorkout() {
+function StartWorkout(props) {
     const classes = useStyles();
+
+    const handleClick = () => {
+        props.isActiveTrue()
+    }
 
     return (
         <div className={classes.root}>
-            <ColorButton variant="contained" color="primary" className={classes.button}>
+            <ColorButton
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={handleClick}
+            >
                 Start Workout
             </ColorButton>
         </div>
     );
 }
+
+const mapDispatchToProps = dispatch => ({
+    isActiveTrue: () => dispatch({ type: IS_ACTIVE_TRUE })
+})
+
+export default connect(null, mapDispatchToProps)(StartWorkout)
