a19512d27e793930a6bd0388d2c0348ea6b84194
jamesncox <jamesncox@yahoo.com>
2020-07-25 21:24:35 -0500

    Update Subtitle.js

--- src/components/Layout/Subtitle.js
+++ src/components/Layout/Subtitle.js
@@ -5,11 +5,9 @@ import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        // marginTop: theme.spacing(1),
         textAlign: "center"
     },
     subtitle: {
-        // fontSize: "1.2rem",
         fontFamily: "'Exo', sans-serif",
         color: "#ba68c8"
     },
@@ -20,33 +18,17 @@ export default function Subtitle(props) {
 
     return (
         <Box className={classes.root}>
-            {/* <Typography className={classes.subtitle}>
-                Fit in a quick workout!
-            </Typography>
-            <Typography className={classes.subtitle}>
-                Select a number of moves
-            </Typography>
-            <Typography className={classes.subtitle}>
-                and how long each move lasts.
-            </Typography>
-            <Typography className={classes.subtitle}>
-                Make sure to warm up first!
-            </Typography> */}
-
             <Typography className={classes.subtitle}>
                 Customize your workouts
             </Typography>
             <Typography className={classes.subtitle}>
-                Never the same
+                Never the same routine
             </Typography>
             <Typography className={classes.subtitle}>
                 No equipment necessary
             </Typography>
             <Typography className={classes.subtitle}>
-                (except a water bottle)
-            </Typography>
-            <Typography className={classes.subtitle}>
-                Now let's Gif Fit!
+                Time to GIF FIT
             </Typography>
         </Box>
     )
