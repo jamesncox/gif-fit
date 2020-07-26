be22a090b2b09239fee0ed55cdf363963abea567
jamesncox <jamesncox@yahoo.com>
2020-07-25 22:39:46 -0500

    Update number of options SelectExerciseNumber / SelectRoundNumber

--- src/components/Layout/SelectExerciseNumber.js
+++ src/components/Layout/SelectExerciseNumber.js
@@ -63,17 +63,21 @@ function SelectExerciseNumber(props) {
                     onChange={handleChange}
                     autoWidth={true}
                 >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
                     <MenuItem value={5}>5</MenuItem>
                     <MenuItem value={6}>6</MenuItem>
                     <MenuItem value={7}>7</MenuItem>
                     <MenuItem value={8}>8</MenuItem>
                     <MenuItem value={9}>9</MenuItem>
                     <MenuItem value={10}>10</MenuItem>
-                    <MenuItem value={11}>11</MenuItem>
+                    {/* <MenuItem value={11}>11</MenuItem>
                     <MenuItem value={12}>12</MenuItem>
                     <MenuItem value={13}>13</MenuItem>
                     <MenuItem value={14}>14</MenuItem>
-                    <MenuItem value={15}>15</MenuItem>
+                    <MenuItem value={15}>15</MenuItem> */}
                 </Select>
             </FormControl>
         </div>
