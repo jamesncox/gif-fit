1ca988a66fc36305c11d76ce6a11b0b30c3ff853
jamesncox <jamesncox@yahoo.com>
2020-07-27 22:19:47 -0500

    Update DisplayGif.js

--- src/components/Gifs/DisplayGif.js
+++ src/components/Gifs/DisplayGif.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DisplayGif(props) {
     const classes = useStyles();
-    const img = require.context('../../assets/', true);
+    const img = require.context('../../assets/', false, /\.(gif)$/)
 
     return (
         <Card className={classes.root} >
