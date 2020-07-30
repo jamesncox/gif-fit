19b3e04328a323f21bd48fbc25815cff7384117d
jamesncox <jamesncox@yahoo.com>
2020-07-30 18:43:29 -0500

    Remove preloaded audio from App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Layout/Home'
-// import UIfx from 'uifx'
-// import StartBeep from './sounds/triple start beep.wav'
-// import Beep from './sounds/very short beep low.wav'
-
-// const startBeep = new UIfx(
-//   StartBeep,
-//   {
-//     volume: .0001,
-//     throttleMs: 50
-//   }
-// )
-
-// const beep = new UIfx(
-//   Beep,
-//   {
-//     volume: .0001,
-//     throttleMs: 50
-//   }
-// )
-
-// const doNothing = () => {
-//   beep.play()
-//   startBeep.play()
-// }
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* {doNothing()} */}
     </div>
   );
 }
