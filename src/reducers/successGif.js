3859541d02a2842312abbfbb60ae7eba87f886c2
jamesncox <jamesncox@yahoo.com>
2020-08-05 09:21:00 -0500

    Add successGif reducer

---
+++ src/reducers/successGif.js
@@ -0,0 +1,28 @@
+import {
+    SET_SUCCESS_GIF
+} from '../actionTypes'
+
+import successMessage from '../data/successMessage.json'
+
+export default (state = {
+    successGif: {}
+}, action) => {
+    switch (action.type) {
+
+        case SET_SUCCESS_GIF:
+            function shuffle(a) {
+                for (let i = a.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [a[i], a[j]] = [a[j], a[i]];
+                }
+                return a.slice(0, 1);
+            }
+
+            const randomGif = shuffle(successMessage)
+            console.log(randomGif)
+            return { ...state, successGif: randomGif }
+
+        default:
+            return state
+    }
+}
