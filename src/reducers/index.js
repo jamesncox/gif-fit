58b3873d5a4a177db052edbd8e2486903bdc5f97
jamesncox <jamesncox@yahoo.com>
2020-08-05 09:21:55 -0500

    Add successGif to reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import params from './params'
 import isActive from './isActive'
+import successGif from './successGif'
 
 
 const rootReducer = combineReducers(
     {
         params,
-        isActive
+        isActive,
+        successGif
     }
 )
 
